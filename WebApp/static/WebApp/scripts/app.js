303dd884a8a96599be2579151be7d30f14ea5b22
Mohit Mehta <mohit.mehta@hindustantimes.com>
2015-08-21 09:26:34 +0530

    frontend - chanfweda

--- WebApp/static/WebApp/scripts/app.js
+++ WebApp/static/WebApp/scripts/app.js
@@ -1,1 +1,44 @@
-angular.module("eventify");//,['ngCookies']
+(function(){
+
+	var eventify = angular.module("eventify", ,['ngRoute']);
+
+	eventify.config(['$routeProvider', 
+	    function($routeProvider){
+
+	        $routeProvider.
+
+	        when('/',{
+
+	            templateUrl: 'home',
+	            controller: 'mainController'
+
+	        }).
+	        when('/search',{
+
+	            templateUrl: 'search',
+	            controller: 'searchController'
+
+	        }).
+	        when('/profile',{
+
+	            templateUrl: 'profile',
+	            controller: 'profileController'
+
+	        }).
+	        when('/myfav',{
+
+	            templateUrl: 'myfav',
+	            controller: 'myfavController'
+
+	        }).
+	        otherwise({
+
+	            redirectTo: '/'
+
+	        });
+
+
+	    }]);
+
+
+})();
---
+++ WebApp/static/WebApp/scripts/controllers/myfavController.js
@@ -0,0 +1,14 @@
+(function(){
+
+
+    var eventify = angular.module("eventify");
+
+    eventify.controller('myfavController', ['$scope', '$http', function ($scope, $http) {
+
+        var host =  'localhost';
+        var port = '8000';
+
+
+      }]);
+
+})();
